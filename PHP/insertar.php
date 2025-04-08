4d381e55cc16b80b0b133fcf392c68d4e8e1df26
Andre <146364284+AndrenorAndiarson@users.noreply.github.com>
2025-04-08 11:50:53 -0400

    Add files via upload

--- PHP/insertar.php
+++ PHP/insertar.php
@@ -2,15 +2,24 @@
 
 include_once('conexion.php');
 
+if($_SERVER['REQUEST_METHOD']== 'POST'){
+    $nombre=$_POST['nombre'];
+    $apellidos=$_POST['apellidos'];
+    $email=$_POST['email'];
+    $direccion=$_POST['direccion'];
+    $telefono=$_POST['telefono'];
+    
+}
 
-$sql = "INSERT INTO Empleados (Nombre,Apellidos)
+$sql = "INSERT INTO Empleados (Nombre,Apellidos,email,direccion,telefono) 
+VALUES ('$nombre','$apellidos','$email','$direccion','$telefono')";
 
-VALUES('Pedro','Garcia');";
 
 
 
 if ($conexion->query($sql) === TRUE) {
-
+header("Location: index.php");
+exit();
     echo "Registro ingresado correctamente.";
 } else {
 
